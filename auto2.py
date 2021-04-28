75f5ee07f2e26ac680664dd5489a0adee74ac0c1
worldwildwap <worldwildwap@gmail.com>
2021-04-28 10:51:55 +0900

    Create auto2.py

---
+++ auto2.py
@@ -0,0 +1,67 @@
+import time
+import pyupbit
+import datetime
+
+access = "JdxnAh46zx6RF97U43LBshxnKpkAX8aRg2tlDAqw"
+secret = "WjfaCl1tDeMSjLyxkQOxEMufI7u36SWzZ5nFOfwD"
+
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_ma15(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# 자동매매 시작
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-SRM") #9:00
+        end_time = start_time + datetime.timedelta(days=1) #9:00 + 1일
+
+        # 9:00 < 현재 < #8:59:50
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-SRM", 0.3)
+            ma15 = get_ma15("KRW-SRM")
+            current_price = get_current_price("KRW-SRM")
+            if target_price < current_price and ma15 < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-SRM", krw*0.9995)
+        else:
+            srm = get_balance("SRM")
+            if srm > 0.42444:
+                upbit.sell_market_order("KRW-SRM", srm*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
